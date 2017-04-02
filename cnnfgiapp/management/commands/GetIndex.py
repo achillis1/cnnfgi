c5af0015a2b7927d0b3cb55dfea2dbd75e475997
Ding Li <dli@nexant.com>
2017-04-02 00:16:07 -0500

    GetIndex is working now

--- cnnfgiapp/management/commands/GetIndex.py
+++ cnnfgiapp/management/commands/GetIndex.py
@@ -21,9 +21,6 @@ from cnnfgiapp.management.commands import SendEmail_Base
 
 from django.utils import timezone
 
-#python manage.py   SendEmail     email    content_id      general_param
-#                 (mgt command)   (To:)    (various)         (various)
-
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
@@ -32,15 +29,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         phantomjs_path = "/usr/local/bin/phantomjs"
         driver = webdriver.PhantomJS(executable_path=phantomjs_path, service_log_path=os.path.devnull)
-
-        # driver = webdriver.Firefox()
         driver.wait = WebDriverWait(driver, 5)
         driver.get("http://money.cnn.com/data/fear-and-greed/")
         try:
             cr = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#needleChart")))
             indexes = cr.find_elements_by_tag_name("li")
             for index in indexes:
-                # print(index.get_attribute("innerHTML"))
                 f1 = index.get_attribute("innerHTML").find("Greed Now:")
                 f2 = index.get_attribute("innerHTML").find("Greed Previous Close:")
                 f3 = index.get_attribute("innerHTML").find("Greed 1 Week Ago:")
@@ -58,19 +52,17 @@ class Command(BaseCommand):
                 if f5!=-1:
                     fgi_one_year_ago = index.get_attribute("innerHTML")[f5+18:f5+18+2]
 
-                fgi_index = Fgi(
-                    index=index.get_attribute("innerHTML")[f1+11:f1+11+2] if f1!=-1 else '',
-                    previous_close=index.get_attribute("innerHTML")[f2+22:f2+22+2] if f2!=-1 else '',
-                    one_week_ago=index.get_attribute("innerHTML")[f3+18:f3+18+2] if f3!=-1 else '',
-                    one_month_ago=index.get_attribute("innerHTML")[f4+19:f4+19+2] if f4!=-1 else '',
-                    one_year_ago=index.get_attribute("innerHTML")[f5+18:f5+18+2] if f5!=-1 else '',
-                    week_day=
-                    )
-            return index.get_attribute("innerHTML")
+            fgi_index = Fgi(
+                index=fgi_now,
+                previous_close=fgi_previous_close,
+                one_week_ago=fgi_one_week_ago,
+                one_month_ago=fgi_one_month_ago,
+                one_year_ago=fgi_one_year_ago,
+                week_day=timezone.now().weekday()
+                )
+            fgi_index.save()
+
         except TimeoutException:
-            print("Get greed error")
-
-
+            pass
 
         driver.quit()
-
